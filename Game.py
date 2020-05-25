e58cddac06d043a59cecce68020a8aadba35581e
CammieVolsci <camillap@id.uff.br>
2020-05-25 16:42:30 -0300

    indo

--- Game.py
+++ Game.py
@@ -1,8 +1,9 @@
-import pygame, Jogador
+import pygame, Jogador, math
 from pygame.locals import *
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+CORES_BLOCOS = ["assets/barrinha1.png","assets/barrinha2.png","assets/barrinha3.png","assets/barrinha4.png","assets/barrinha5.png"]
 
 class BreakoutGame:
 
@@ -11,14 +12,16 @@ class BreakoutGame:
     background = None
     displaysurf = None 
     run = True
-    fps = None
+    fps = 30
     game_over = False
+    total_blocos = 45
 
     basic_font = None
     small_font = None
 
     jogador = None
     bolinha = None
+    bloco = []
 
     def __init__(self):
         self.window_width = 800
@@ -41,28 +44,41 @@ class BreakoutGame:
                 if k == K_ESCAPE:
                     self.run = False
                 if k == K_LEFT:
-                    jogador.mover = -8
+                    jogador.mover = -9
                 elif k == K_RIGHT:
-                    jogador.mover = 8
+                    jogador.mover = 9
             elif t == KEYUP:
                 if k == K_LEFT or k== K_RIGHT:
                     jogador.mover = 0
     
     def actors_update(self):
         jogador = self.jogador
+        bolinha = self.bolinha
+
+        if jogador.teste_colisao(bolinha):
+            bolinha.mover_y *= -1
+
         jogador.movimento()
+        bolinha.movimento()
 
     def actors_draw(self):
         jogador = self.jogador
         bolinha = self.bolinha
+        bloco = self.bloco
 
         jogador.desenhar(self.displaysurf)
         bolinha.desenhar(self.displaysurf)
+
+        for i in range(self.total_blocos):
+            bloco[i].desenhar(self.displaysurf)
 
     def loop(self):
         fpsclock = pygame.time.Clock()   
         self.jogador = Jogador.Paddle()
         self.bolinha = Jogador.Ball()
+
+        for i in range(self.total_blocos):
+            self.bloco.append(Jogador.Blocks(1 + 90*(i%9),50 + 35*math.floor(i/9),CORES_BLOCOS[math.floor(i/9)]))
 
         while self.run:
             self.displaysurf.fill(BLACK) 
--- Jogador.py
+++ Jogador.py
@@ -1,9 +1,9 @@
-import pygame
+import pygame, datetime, random
 
 PLAYER_IMAGE = "assets/paddle.png"
 BALL_IMAGE = "assets/ball.png"
 
-class Paddle():
+class Paddle(pygame.sprite.Sprite):
 
     def __init__(self):
         self.x = 340
@@ -27,17 +27,55 @@ class Paddle():
         
         self.rect.x = self.x
         self.rect.y = self.y
+    
+    def teste_colisao(self,sprite):
+        if(self.image!=0):
+            return self.rect.colliderect(sprite.rect)            
 
 class Ball():
 
+    random.seed(datetime.time())  
+
     def __init__(self):
-        self.x = 380
-        self.y = 550
-        self.mover = 0
+        self.x = random.randint(100,700)
+        self.y = random.randint(300,550)
+        self.mover_x = 7 * pow(-1,random.randint(1,2))
+        self.mover_y = 7 * pow(-1,random.randint(1,2))
         self.image = pygame.image.load(BALL_IMAGE)
         self.rect = self.image.get_rect() 
         self.rect.x = self.x
         self.rect.y = self.y
 
     def desenhar(self,screen):
-            screen.blit(self.image,(self.x,self.y))
+        screen.blit(self.image,(self.x,self.y))
+
+    def movimento(self):                 
+        self.x += self.mover_x
+        self.y += self.mover_y
+
+        if self.x <= 0:
+            self.mover_x *= -1
+        elif self.x >= 775:
+            self.mover_x *= -1
+
+        if self.y <= 0:
+            self.mover_y *= -1
+        elif self.y >= 625:
+            self.x = random.randint(100,700)
+            self.y = random.randint(300,550)
+        
+        self.rect.x = self.x
+        self.rect.y = self.y       
+
+class Blocks():
+
+    def __init__(self,x,y,image):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def desenhar(self,screen):
+        screen.blit(self.image,(self.x,self.y))   
