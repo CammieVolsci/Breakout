1f45613b9bc2d6f1a85cf155cffbe1f9976752c2
CammieVolsci <camillap@id.uff.br>
2020-05-26 17:01:36 -0300

    70%?

--- Game.py
+++ Game.py
@@ -13,6 +13,7 @@ class BreakoutGame:
     displaysurf = None 
     run = True
     fps = 30
+
     game_over = False
     total_blocos = 45
 
@@ -29,7 +30,9 @@ class BreakoutGame:
         self.fps = 30   
         pygame.init()
         pygame.display.set_caption("Breakout")
-        self.displaysurf = pygame.display.set_mode((self.window_width,self.window_height))  
+        self.displaysurf = pygame.display.set_mode((self.window_width,self.window_height))
+        self.basic_font = pygame.font.Font('freesansbold.ttf',32)     
+        self.small_font = pygame.font.Font('freesansbold.ttf',20)    
 
     def handle_events(self):
         jogador = self.jogador
@@ -43,9 +46,11 @@ class BreakoutGame:
             elif t == KEYDOWN:
                 if k == K_ESCAPE:
                     self.run = False
-                if k == K_LEFT:
+                if k == K_r and self.game_over:
+                    self.reset()
+                if k == K_LEFT and not self.game_over:
                     jogador.mover = -9
-                elif k == K_RIGHT:
+                elif k == K_RIGHT and not self.game_over:
                     jogador.mover = 9
             elif t == KEYUP:
                 if k == K_LEFT or k== K_RIGHT:
@@ -54,12 +59,25 @@ class BreakoutGame:
     def actors_update(self):
         jogador = self.jogador
         bolinha = self.bolinha
+        bloco = self.bloco
+
+        if bolinha.lost == True:
+            jogador.vidas -= 1
+            bolinha.lost = False
 
         if jogador.teste_colisao(bolinha):
             bolinha.mover_y *= -1
 
+        for i in range(self.total_blocos):
+            if bloco[i].rect!=0 and bolinha.teste_colisao(bloco[i]):
+                bloco[i].kill()
+                jogador.pontuacao += 10
+                bolinha.mover_y *= -1
+
         jogador.movimento()
-        bolinha.movimento()
+
+        if not self.game_over:
+            bolinha.movimento()
 
     def actors_draw(self):
         jogador = self.jogador
@@ -70,7 +88,20 @@ class BreakoutGame:
         bolinha.desenhar(self.displaysurf)
 
         for i in range(self.total_blocos):
-            bloco[i].desenhar(self.displaysurf)
+            if(bloco[i].image!=0):
+                bloco[i].desenhar(self.displaysurf)
+    
+    def reset(self):
+        self.bloco.clear()
+        self.jogador.pontuacao = 0
+        self.jogador.vidas = 5
+        self.bolinha.x = 400
+        self.bolinha.y = 350
+
+        for i in range(self.total_blocos):
+            self.bloco.append(Jogador.Blocks(1 + 90*(i%9),35 + 35*math.floor(i/9),CORES_BLOCOS[math.floor(i/9)]))
+
+        self.game_over = False
 
     def loop(self):
         fpsclock = pygame.time.Clock()   
@@ -78,10 +109,36 @@ class BreakoutGame:
         self.bolinha = Jogador.Ball()
 
         for i in range(self.total_blocos):
-            self.bloco.append(Jogador.Blocks(1 + 90*(i%9),50 + 35*math.floor(i/9),CORES_BLOCOS[math.floor(i/9)]))
+            self.bloco.append(Jogador.Blocks(1 + 90*(i%9),35 + 35*math.floor(i/9),CORES_BLOCOS[math.floor(i/9)]))
+
+        gameOverSurf = self.basic_font.render('Game Over',True,WHITE)
+        gameOverRect = gameOverSurf.get_rect()
+        gameOverRect.center = (405,250)   
+
+        resetSurf = self.basic_font.render('Pressione R para reiniciar',True,WHITE)
+        resetRect = resetSurf.get_rect()
+        resetRect.center = (405,300)  
 
         while self.run:
+            pontuacao_txt = str(self.jogador.pontuacao) 
+            vidas_txt = str(self.jogador.vidas)
+
+            pontuacaoSurf = self.small_font.render('PONTOS: ' + pontuacao_txt,True,WHITE)
+            pontuacaoRect = pontuacaoSurf.get_rect()
+            pontuacaoRect.center = (70,20)
+
+            vidasSurf = self.small_font.render('VIDAS: ' + vidas_txt,True,WHITE)
+            vidasRect = vidasSurf.get_rect()
+            vidasRect.center = (740,20)
+
             self.displaysurf.fill(BLACK) 
+            self.displaysurf.blit(pontuacaoSurf,pontuacaoRect) 
+            self.displaysurf.blit(vidasSurf,vidasRect)
+
+            if self.jogador.vidas <= 0:
+                self.game_over = True
+                self.displaysurf.blit(gameOverSurf,gameOverRect)
+                self.displaysurf.blit(resetSurf,resetRect)
 
             self.handle_events()
             self.actors_update()
--- Jogador.py
+++ Jogador.py
@@ -7,12 +7,14 @@ class Paddle(pygame.sprite.Sprite):
 
     def __init__(self):
         self.x = 340
-        self.y = 580
+        self.y = 600
         self.mover = 0
         self.image = pygame.image.load(PLAYER_IMAGE)
         self.rect = self.image.get_rect() 
         self.rect.x = self.x
         self.rect.y = self.y
+        self.pontuacao = 0
+        self.vidas = 5
     
     def desenhar(self,screen):
         screen.blit(self.image,(self.x,self.y))
@@ -32,7 +34,7 @@ class Paddle(pygame.sprite.Sprite):
         if(self.image!=0):
             return self.rect.colliderect(sprite.rect)            
 
-class Ball():
+class Ball(pygame.sprite.Sprite):
 
     random.seed(datetime.time())  
 
@@ -40,11 +42,12 @@ class Ball():
         self.x = random.randint(100,700)
         self.y = random.randint(300,550)
         self.mover_x = 7 * pow(-1,random.randint(1,2))
-        self.mover_y = 7 * pow(-1,random.randint(1,2))
+        self.mover_y = -7 
         self.image = pygame.image.load(BALL_IMAGE)
         self.rect = self.image.get_rect() 
         self.rect.x = self.x
         self.rect.y = self.y
+        self.lost = False
 
     def desenhar(self,screen):
         screen.blit(self.image,(self.x,self.y))
@@ -61,11 +64,16 @@ class Ball():
         if self.y <= 0:
             self.mover_y *= -1
         elif self.y >= 625:
+            self.lost = True
             self.x = random.randint(100,700)
             self.y = random.randint(300,550)
         
         self.rect.x = self.x
-        self.rect.y = self.y       
+        self.rect.y = self.y 
+
+    def teste_colisao(self,sprite):
+        if(self.image!=0):
+            return self.rect.colliderect(sprite.rect)                 
 
 class Blocks():
 
@@ -78,4 +86,10 @@ class Blocks():
         self.rect.y = self.y
 
     def desenhar(self,screen):
-        screen.blit(self.image,(self.x,self.y))   
+        screen.blit(self.image,(self.x,self.y))  
+
+    def kill(self):
+        self.x = 0
+        self.y = 0
+        self.image = 0
+        self.rect = 0
