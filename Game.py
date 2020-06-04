157de490321631f1bb9acdeee2a85206fd529e48
CammieVolsci <camillap@id.uff.br>
2020-06-04 15:05:15 -0300

    ok

--- Game.py
+++ Game.py
@@ -11,61 +11,99 @@ class StateMachine:
 
 class Textos:
 
-    gameOverSurf = None
-    gameOverRect = None
-
-    resetSurf = None
-    resetRect = None
-
-    pontuacaoSurf = None
-    pontuacaoRect = None
-
-    vidasSurf = None
-    vidasRect = None
-
-    menuSurf = None
-    menuRect = None
-
     white = (255,255,255)
-
+    
     def __init__(self):
         pygame.init()          
         self.basic_font = pygame.font.Font('freesansbold.ttf',32)     
         self.small_font = pygame.font.Font('freesansbold.ttf',20)  
 
-    def gameover(self):
-        self.gameOverSurf = self.basic_font.render('Game Over',True,self.white)
-        self.gameOverRect = self.gameOverSurf.get_rect()
-        self.gameOverRect.center = (405,250)   
+    def gameover(self,displaysurf):
+        gameOverSurf = self.basic_font.render('Game Over',True,self.white)
+        resetSurf = self.basic_font.render('Pressione ENTER para reiniciar',True,self.white)
+        gameOverRect = gameOverSurf.get_rect()
+        resetRect = resetSurf.get_rect()
+        gameOverRect.center = (405,250)
+        resetRect.center = (405,300)    
+        displaysurf.blit(gameOverSurf,gameOverRect)             
+        displaysurf.blit(resetSurf,resetRect)
 
-    def reset(self):
-        self.resetSurf = self.basic_font.render('Pressione R para reiniciar',True,self.white)
-        self.resetRect = self.resetSurf.get_rect()
-        self.resetRect.center = (405,300) 
+    def pontuacao(self,pontuacao_txt,displaysurf): 
+        pontuacaoSurf = self.small_font.render('PONTOS: ' + pontuacao_txt,True,self.white)
+        pontuacaoRect = pontuacaoSurf.get_rect()
+        pontuacaoRect.center = (70,20)
+        displaysurf.blit(pontuacaoSurf,pontuacaoRect)
 
-    def pontuacao(self,pontuacao_txt): 
-        self.pontuacaoSurf = self.small_font.render('PONTOS: ' + pontuacao_txt,True,self.white)
-        self.pontuacaoRect = self.pontuacaoSurf.get_rect()
-        self.pontuacaoRect.center = (70,20)
+    def vidas(self,vidas_txt,displaysurf):
+        vidasSurf = self.small_font.render('VIDAS: ' + vidas_txt,True,self.white)
+        vidasRect = vidasSurf.get_rect()
+        vidasRect.center = (740,20)
+        displaysurf.blit(vidasSurf,vidasRect)
 
-    def vidas(self,vidas_txt):
-        self.vidasSurf = self.small_font.render('VIDAS: ' + vidas_txt,True,self.white)
-        self.vidasRect = self.vidasSurf.get_rect()
-        self.vidasRect.center = (740,20)
+    def menu_txt(self,displaysurf):
+        logoSurf = self.basic_font.render('BREAKOUT',True,self.white)
+        enterSurf = self.basic_font.render('Press ENTER',True,self.white)
+        logoRect = logoSurf.get_rect()
+        enterRect = enterSurf.get_rect()
+        logoRect.center = (400,300)
+        enterRect.center = (400,350)         
+        displaysurf.blit(logoSurf,logoRect)
+        displaysurf.blit(enterSurf,enterRect)
 
-    def menu_txt(self):
-        self.menuSurf = self.basic_font.render('BREAKOUT',True,self.white)
-        self.menuRect = self.menuSurf.get_rect()
-        self.menuRect.center = (400,300)
+    def input_box(self,displaysurf,nome_jogador):
+        text_box = pygame.Rect(205,380,50,30)
+        textSurf = self.small_font.render(nome_jogador,True,self.black)  
+        scoreSurf = self.small_font.render('Digite o nome para a pontuação máxima:',True,self.white)
+        scoreRect = scoreSurf.get_rect()
+        scoreRect.center = (405,350)
+        width = max(200,textSurf.get_width()+10)
+        text_box.w = width
+        displaysurf.blit(scoreSurf,scoreRect)
+        pygame.draw.rect(displaysurf,self.white,text_box,0)
+        displaysurf.blit(textSurf,(text_box.x+5,text_box.y+5))
 
-class MainMenu(StateMachine,Textos):
+    def maxscore_menu(self,displaysurf,lista):
+        scoreSurf = self.basic_font.render('Pontuações máximas:',True,self.white)
+        scoreRect = scoreSurf.get_rect()
+        scoreRect.center(50,50)
+
+
+class ScoreMenu(StateMachine):
     
     black = (0,0,0) 
+    file = None
+    lista = []
 
     def __init__(self):
         StateMachine.__init__(self)
-        Textos.__init__(self)
-        self.next = 'game'
+        self.next = 'menu'
+
+    def startup(self):
+        pass
+
+    def cleanup(self):
+        pass
+
+    def handle_events(self,event):
+        pass
+
+    def update(self,displaysurf):
+        t = Textos()
+        displaysurf.fill(self.black)
+
+        self.file = open ('max_scores','r')
+        count = len(open('max_scores').readlines())
+        for i in range(count):
+            self.lista.append(self.file.readline())
+        self.file.close()
+    
+
+class MainMenu(StateMachine):  
+
+    black = (0,0,0) 
+
+    def __init__(self):
+        StateMachine.__init__(self)        
 
     def startup(self):
         pass
@@ -75,33 +113,42 @@ class MainMenu(StateMachine,Textos):
 
     def handle_events(self,event):
         if event.type == KEYDOWN:
-            if event.key == K_x:
+            if event.key == K_RETURN or event.key == K_KP_ENTER:
+                self.next = 'game'
+                self.end = True
+            elif event.key == K_BACKSPACE:
+                self.next = 'score'
                 self.end = True
 
     def update(self,displaysurf):
-        self.menu_txt()
-        displaysurf.fill(self.black) 
-        displaysurf.blit(self.menuSurf,self.menuRect)
+        t = Textos()
 
-class Game(StateMachine,Textos):
+        displaysurf.fill(self.black)
+        t.menu_txt(displaysurf)        
+
+class Game(StateMachine):
    
     background = None
-    game_over = False
-    total_blocos = 45
+    game_over = False  
     black = (0,0,0) 
 
     jogador = None
     bolinha = None
     bloco = []
+
+    total_blocos = 45  
+    pontuacao_max = 0
+    nome_jogador = ''
+    file = None
+    lista = []
     
     def __init__(self,**game_images):
         StateMachine.__init__(self)
         Textos.__init__(self)
-        self.__dict__.update(game_images)        
+        self.__dict__.update(game_images)                    
 
-    def startup(self):
-        random.seed(datetime.time())   
-
+    def startup(self):      
+        random.seed(datetime.time())
         self.jogador = actors.Paddle(340,600,self.player_image)
         self.bolinha = actors.Ball(random.randint(300,600),random.randint(250,400),self.ball_image)
 
@@ -109,18 +156,38 @@ class Game(StateMachine,Textos):
             self.bloco.append(actors.Blocks(1 + 90*(i%9),35 + 35*math.floor(i/9),self.cores_blocos[math.floor(i/9)]))
 
     def cleanup(self):
-        pass  
+        self.bloco.clear()
+        self.jogador.pontuacao = 0
+        self.jogador.vidas = 5
+        self.bolinha.x = 400
+        self.bolinha.y = 350
+        self.nome_jogador = ''
+        self.pontuacao_max = 0
+
+        for i in range(self.total_blocos):
+            self.bloco.append(actors.Blocks(1 + 90*(i%9),35 + 35*math.floor(i/9),self.cores_blocos[math.floor(i/9)]))
+
+        self.game_over = False
+        self.lista.clear()
 
     def handle_events(self,event):
         jogador = self.jogador
 
         if event.type == KEYDOWN:
-            if event.key == K_r and self.game_over:
-                self.reseta_game()
-            if event.key == K_LEFT and not self.game_over:
-                jogador.mover = -9
-            elif event.key == K_RIGHT and not self.game_over:
-                jogador.mover = 9
+            if self.game_over:
+                if event.key == K_RETURN or event.key == K_KP_ENTER:
+                    self.salvar_pontuacaomax() 
+                    self.next = 'menu'                   
+                    self.end = True
+                if event.key == pygame.K_BACKSPACE:
+                    self.nome_jogador = self.nome_jogador[:-1] 
+                else:
+                    self.nome_jogador += event.unicode             
+            else:      
+                if event.key == K_LEFT:
+                    jogador.mover = -9
+                elif event.key == K_RIGHT:
+                    jogador.mover = 9
         elif event.type == KEYUP:
             if event.key == K_LEFT or event.key == K_RIGHT:
                 jogador.mover = 0
@@ -158,42 +225,44 @@ class Game(StateMachine,Textos):
 
         for i in range(self.total_blocos):
             if(bloco[i].image!=0):
-                bloco[i].desenhar(displaysurf)
-    
-    def reseta_game(self):
-        self.bloco.clear()
-        self.jogador.pontuacao = 0
-        self.jogador.vidas = 5
-        self.bolinha.x = 400
-        self.bolinha.y = 350
-
-        for i in range(self.total_blocos):
-            self.bloco.append(actors.Blocks(1 + 90*(i%9),35 + 35*math.floor(i/9),self.cores_blocos[math.floor(i/9)]))
-
-        self.game_over = False
+                bloco[i].desenhar(displaysurf)         
 
     def update(self,displaysurf):   
 
+        t = Textos()
+
         pontuacao_txt = str(self.jogador.pontuacao) 
         vidas_txt = str(self.jogador.vidas)
-
-        self.gameover()
-        self.reset()       
-        self.pontuacao(pontuacao_txt)
-        self.vidas(vidas_txt)       
-
+   
         displaysurf.fill(self.black) 
-        displaysurf.blit(self.pontuacaoSurf,self.pontuacaoRect) 
-        displaysurf.blit(self.vidasSurf,self.vidasRect)
+        t.pontuacao(pontuacao_txt,displaysurf)
+        t.vidas(vidas_txt,displaysurf)
 
         if self.jogador.vidas <= 0:
-            self.game_over = True
-            displaysurf.blit(self.gameOverSurf,self.gameOverRect)
-            displaysurf.blit(self.resetSurf,self.resetRect)
+            self.pontuacao_max = self.jogador.pontuacao  
+            self.game_over = True          
+            t.gameover(displaysurf)
+            t.input_box(displaysurf,self.nome_jogador)                     
 
         self.actors_update()
-        self.actors_draw(displaysurf)       
-            
+        self.actors_draw(displaysurf)  
+
+    def salvar_pontuacaomax(self):
+
+        self.file = open ('max_scores','r')
+        count = len(open('max_scores').readlines())
+        for i in range(count):
+            self.lista.append(self.file.readline())
+        self.file.close()
+
+        self.lista.pop(9)
+        self.lista.insert(0,self.nome_jogador + " " + str(self.pontuacao_max) + "\n")
+
+        self.file = open ('max_scores','w')
+        for i in range(len(self.lista)):
+            self.file.write(self.lista[i])
+        self.file.close()       
+                    
 class BreakoutGame:
 
     state_dictionary = None
@@ -265,6 +334,7 @@ def main():
     dicionario_estados = {
         'menu' : MainMenu(),
         'game' : Game(**game_images),
+        'score' : ScoreMenu()
     }
 
     main_game = BreakoutGame(**settings)
